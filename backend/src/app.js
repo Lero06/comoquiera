d8885a1fa79f4ac973563b0956f185871050148e
Leandro Sanchez Rojas <lesanchezro@est.utn.ac.cr>
2026-06-16 11:47:02 -0600

    Modulo Controllers y Routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import parqueoRoutes from "./routes/parqueo.routes.js"
 dotenv.config();
 
 const NAME=process.env.SERVER_NAME;
@@ -25,41 +26,7 @@ app.get("/",(req,res)=>{
     })
 });
 
-app.post("/api/parqueo/calcular",(req,res)=>{
-    const {PLACA,TIPO,HORAS,MINUTOS}=req.body;
-
-    if(!PLACA ||PLACA.trim()==""){
-        res.status(400).json({error:"La placa es requerida"})
-    }
-    if(!TIPO ||(TIPO !== "carro" && TIPO !== "moto")){
-        res.status(400).json({error:"El tipo es requerido"})
-    }
-    if(Number.isNaN(HORAS) || HORAS <0){
-        res.status(400).json({error:"La horas son requeridas"})
-    }
-    if(Number.isNaN(MINUTOS) || MINUTOS <0|| MINUTOS >=60){
-        res.status(400).json({error:"Los minutos son requeridas"})
-    }
-
-    const TARIFA=TIPO==="carro"? 1200 : 500;
-
-    let h=Number(HORAS);
-    let m=Number(MINUTOS);
-
-    if(m>5) h++;
-
-    const TOTAL=h*TARIFA;
-
-    res.json({
-        placa: PLACA,
-        tipo: TIPO,
-        tarifa: TARIFA,
-        tiempoUso: HORAS+":"+MINUTOS,
-        horasCobradas: h,
-        total: TOTAL
-    });
-
-})
+app.use("/api/parqueo",parqueoRoutes)
 
 app.listen(4000,()=>{
 
