ce7baac736b0e067751ef5b7eea14fc676d390cb
Leandro Sanchez Rojas <lesanchezro@est.utn.ac.cr>
2026-06-16 09:42:02 -0600

    Muestra .env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const NAME=process.env.SERVER_NAME;
+const VERSION=process.env.SERVER_VERSION;
+const DESCR=process.env.SERVER_DESCRIPTION;
+const PORT=process.env.SERVER_PORT;
+
 
 const app = express();
 
+
+
 app.get("/",(req,res)=>{
 
-    res.send("<h1>Hola</h1>")
+    res.send(`<h1>Nombre del Servidor: ${NAME}</h1>`)
 
 });
 
 app.listen(4000,()=>{
 
-    console.log("listo");
+    console.log("Nombre del servidor: ",NAME);
 });
